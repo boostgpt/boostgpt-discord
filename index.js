ee57acfddefe04a8b71b4c661d4e98456ea2cf76
David Oti <davmixcool@gmail.com>
2025-12-07 13:55:09 +0100

    Refactor: Comment out unused payload properties in chat function

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ client.on('messageCreate', async (message) => {
             provider_key: process.env.OPENAI_API_KEY,
             model: process.env.BOOSTGPT_BOT_MODEL, //The model to use for the chat response. Defaults to the bot model.
             message: message.content, //The chat message
-            source_ids: process.env.BOOSTGPT_BOT_SOURCE_IDS, //The training source id's you want the AI's knowledge to be limited to.
-            tags: process.env.BOOSTGPT_BOT_TAGS, //Use tags to get the segment of the training data you want the AI's knowledge to be limited to.
-            top: process.env.BOOSTGPT_BOT_TOP, //Optional. The weight of training data used to form a context. Defaults to 10. Recommended settings between : 10 - 15 give better response from the AI.
+            //source_ids: process.env.BOOSTGPT_BOT_SOURCE_IDS, //The training source id's you want the AI's knowledge to be limited to.
+            //tags: process.env.BOOSTGPT_BOT_TAGS, //Use tags to get the segment of the training data you want the AI's knowledge to be limited to.
+            //top: process.env.BOOSTGPT_BOT_TOP, //Optional. The weight of training data used to form a context. Defaults to 10. Recommended settings between : 10 - 15 give better response from the AI.
             chat_id: message.author.id != process.env.DISCORD_BOT_ID? 'discord-'+message.author.id : null
         }
 
